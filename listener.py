789158459c98a0f90b0b0e3c104bb73a62a46c61
Felipe <jesfel@gmail.com>
2020-05-20 14:10:11 -0400

    add get method

--- listener.py
+++ listener.py
@@ -3,15 +3,17 @@ from flask import Flask, request, abort
 
 app = Flask(__name__)
 
-@app.route('/',methods=['POST'])
+@app.route('/',methods=['GET', 'POST'])
 def webhook():
     print("webhook")
     sys.stdout.flush()
+    if request.method == 'GET':
+        return '<h1>Saferite Webhook Listener</h1>'
     if request.method == 'POST':
         print(request.json)
-        return '', 200
+        return 'Success', 200
     else:
         abort(400)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
